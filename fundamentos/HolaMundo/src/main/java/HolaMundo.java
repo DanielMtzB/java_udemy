f443d0075f0ab160fb66d704dffc65dac5ab6c33
sayamarber <dany.berumen@gmail.com>
2020-06-09 17:15:01 -0500

    primer tarea, saludo

--- fundamentos/HolaMundo/src/main/java/HolaMundo.java
+++ fundamentos/HolaMundo/src/main/java/HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String args[]){
         System.out.println("Hola Mundo de Java... otra vez UwU");
+        System.out.println(Saludo.saludar("dany"));
     }
 }
